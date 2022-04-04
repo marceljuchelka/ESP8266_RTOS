437e026b82650559f35ed3b1aace695a98ccc117
marcel <marcel@PC-Marcel>
2022-04-04 11:46:53 +0200

    pridani knihoven

--- Blikani_led_Witty/main/main.c
+++ Blikani_led_Witty/main/main.c
@@ -28,35 +28,46 @@
 #define led_pin_blue	GPIO_Pin_13
 
 
-#define led_mb		GPIO_NUM_2
-#define led_red		GPIO_NUM_15
-#define led_green	GPIO_NUM_12
-#define led_blue	GPIO_NUM_13
+#define led_mb_pin		GPIO_NUM_2
+#define led_red_pin		GPIO_NUM_15
+#define led_green_pin	GPIO_NUM_12
+#define led_blue_pin	GPIO_NUM_13
 
-gpio_config_t	gpio_cfg;
+
 adc_config_t	adc_cfg;
 
+void vPartTestInitialize(){
+	gpio_config_t	gpio_cfg;
+	gpio_cfg.pin_bit_mask = led_pin_mb | led_pin_red | led_pin_green | led_pin_blue;
+	gpio_cfg.mode = GPIO_MODE_DEF_OUTPUT;
+	gpio_config(&gpio_cfg);
+	adc_cfg.mode = ADC_READ_TOUT_MODE;
+}
 
+//void vLedToggle( void *pvParameters){
+//	static uint8_t port_masc = 0;
+//	configASSERT(((uint32_t)*pvParameters) == 1);
+//		for(;;) gpio_set_level(led_pin_red, port_masc^=);
+//
+//}
 
 void app_main()
 {
 
 	uint8_t uroven = 0;
-	gpio_cfg.pin_bit_mask = led_pin_mb | led_pin_red | led_pin_green | led_pin_blue;
-	gpio_cfg.mode = GPIO_MODE_DEF_OUTPUT;
-	gpio_config(&gpio_cfg);
-	adc_cfg.mode = ADC_READ_TOUT_MODE;
+
+
 
 //	gpio_set_direction(led_mb, GPIO_MODE_OUTPUT);
 	while(1){
 		uroven^= 1;
-		gpio_set_level(led_green, uroven);
+		gpio_set_level(led_green_pin, uroven);
 		vTaskDelay(100/portTICK_PERIOD_MS);
-		gpio_set_level(led_red, uroven);
+		gpio_set_level(led_red_pin, uroven);
 		vTaskDelay(200/portTICK_PERIOD_MS);
-		gpio_set_level(led_blue, uroven);
+		gpio_set_level(led_blue_pin, uroven);
 		vTaskDelay(300/portTICK_PERIOD_MS);
-		gpio_set_level(led_mb, uroven);
+		gpio_set_level(led_mb_pin, uroven);
 		vTaskDelay(200/portTICK_PERIOD_MS);
 	}
 
