d16bbf1041d3eb641a528d5c9f6dbdb6b3bc8599
marcel <marcel@PC-Marcel>
2022-04-11 07:19:06 +0200

    start vzor

---
+++ Rtos_vzor/main/Rtos_vzor.c
@@ -0,0 +1,71 @@
+/* test preruseni od GPIO
+ * vyuzivam ho na nacitani IR signalu
+*/
+#include <stdio.h>
+#include <string.h>
+//#include "time.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/FreeRTOSConfig.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
+#include "FreeRTOSConfig.h"
+#include "esp_system.h"
+#include "esp_spi_flash.h"
+#include "esp_timer.h"
+#include "esp_task_wdt.h"
+#include "driver/gpio.h"
+#include "driver/uart.h"
+#include "portmacro.h"
+#include "sdkconfig.h"
+
+#define MB_LED	GPIO_NUM_2
+
+TaskHandle_t	BlikLedMBHandle;
+TaskHandle_t	PrintFreeMemoryHandle;
+
+
+void vBlink_Led2(void *arg){
+	gpio_config_t gpio_cfg;
+	gpio_cfg.pin_bit_mask = (1<<MB_LED);
+	gpio_cfg.mode = GPIO_MODE_DEF_OUTPUT;
+	gpio_config(&gpio_cfg);
+	uint8_t uroven=0;
+
+	while(1){
+		gpio_set_level(GPIO_NUM_2, uroven=uroven^1);
+		vTaskDelay(20);
+	}
+}
+
+
+
+void vText2(void *arg){
+while(1){
+	printf("Text2 \n");
+	vTaskDelay(1500/portTICK_PERIOD_MS);
+	}
+}
+
+void vPrintFreeMemory(void *arg) {
+	while (1) {
+		printf("BlikLedMBHandle: %d \n", uxTaskGetStackHighWaterMark(BlikLedMBHandle));
+		printf("vPrintFreeMemory: %d \n", uxTaskGetStackHighWaterMark(NULL));
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+}
+
+
+
+void app_main()
+{
+	printf("start\n");
+	printf("*** senzor ozonu ***\n");
+	vTaskDelay(400);
+
+	xTaskCreate(vPrintFreeMemory, "printfreememory", 4096, NULL, 1, &PrintFreeMemoryHandle);
+	xTaskCreate(vBlink_Led2, "blik led2", 1500, NULL, 1,&BlikLedMBHandle );
+
+
+
+}
