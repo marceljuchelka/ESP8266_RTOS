42a3fe5e256de40f459e02496210f61e5b6adf71
marcel <marcel@PC-Marcel>
2022-04-13 14:37:36 +0200

    pridana knihovna ULP

--- senzor_ozonu/main/senzor_ozonu.c
+++ senzor_ozonu/main/senzor_ozonu.c
@@ -19,6 +19,7 @@
 #include "portmacro.h"
 #include "sdkconfig.h"
 #include "../components/ADS_1115/ads_1115.h"
+#include "../components/ADS_1115/ulp.h"
 //#include "components/ULP/ulp.h"
 #define MB_LED	GPIO_NUM_2
 
@@ -53,8 +54,10 @@ void vPrintFreeMemory(void *arg) {
 	while (1) {
 //		printf("BlikLedMBHandle: %d \n", uxTaskGetStackHighWaterMark(BlikLedMBHandle));
 //		printf("vPrintFreeMemory: %d \n", uxTaskGetStackHighWaterMark(NULL));
-		printf("MUX %d   hodnota: %f \n", MUX, ads_U_input_single(MUX));
-		if(++MUX == 2)MUX = 0;
+//		printf("MUX %d   hodnota: %f \n", MUX, ads_U_input_single(MUX));
+		printf("Baterie %f \n",ULP_Battery_check1());
+		printf("hodnota ozonu je: %f \n", (float)ULP_Vgas_read_PPM());
+//		if(++MUX == 2)MUX = 0;
 		vTaskDelay(300 / portTICK_PERIOD_MS);
 	}
 }
@@ -67,7 +70,8 @@ void app_main()
 	printf("*** senzor ozonu ***\n");
 	vTaskDelay(100);
 	my_i2c_config();
-	ads_init();
+//	ads_init();
+	ULP_init();
 	xTaskCreate(vPrintFreeMemory, "printfreememory", 4096, NULL, 1, &PrintFreeMemoryHandle);
 	xTaskCreate(vBlink_Led2, "blik led2", 1500, NULL, 1,&BlikLedMBHandle );
 
