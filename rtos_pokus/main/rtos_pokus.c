ca2d0e48d313367d036dc5b968cc995e3bda22d5
marcel <marcel@PC-Marcel>
2022-05-02 14:57:59 +0200

    VtaskDelete OK

--- rtos_pokus/main/rtos_pokus.c
+++ rtos_pokus/main/rtos_pokus.c
@@ -13,13 +13,14 @@
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "esp_spi_flash.h"
+#include "esp_log.h"
 #include "driver/gpio.h"
 #include "portmacro.h"
 #include "sdkconfig.h"
 
-TaskHandle_t task1hadle;
-TaskHandle_t task2hadle;
-TaskHandle_t task3hadle;
+TaskHandle_t BlikLedHandle;
+TaskHandle_t Print1Handle;
+TaskHandle_t Print2Handle;
 
 #define MB_LED	GPIO_NUM_2
 
@@ -44,36 +45,49 @@ void vBlink_Led2(void *arg){
 	}
 }
 
-void vText1(void *arg){
-
-while(1){
-	printf("Text1 \n");
-	vTaskDelay(500/portTICK_PERIOD_MS);
-	}
-}
-
-void vText2(void *arg){
-
-while(1){
-	printf("Text2 \n");
-	vTaskDelay(1500/portTICK_PERIOD_MS);
-	}
-}
-
 void vPrintFreeMemory(void *arg) {
 	while (1) {
-		printf("Task1: %d \n", uxTaskGetStackHighWaterMark(task1hadle));
-		printf("Task2: %d \n", uxTaskGetStackHighWaterMark(task2hadle));
-		printf("blikled: %d \n", uxTaskGetStackHighWaterMark(task3hadle));
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		ESP_LOGI("Free Mem:","%d\n", esp_get_free_heap_size());
+		vTaskDelay(100 / portTICK_PERIOD_MS);
 	}
 }
+
+void vText2(void *arg) {
+	ESP_LOGI("print2","created");
+	while (1) {
+		vTaskDelay(300 / portTICK_PERIOD_MS);
+		printf("Text2 \n");
+	}
+}
+
+
+void vText1(void *arg) {
+	while (1) {
+		printf("Text1 \n");
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		xTaskCreate(vText2, "text2", 2048, NULL, 1, &Print2Handle);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+		ESP_LOGI("print2","kill");
+		vTaskDelete(Print2Handle);
+	}
+}
+
+
+
+//void vPrintFreeMemory(void *arg) {
+//	while (1) {
+//		printf("Task1: %d \n", uxTaskGetStackHighWaterMark(task1hadle));
+//		printf("Task2: %d \n", uxTaskGetStackHighWaterMark(task2hadle));
+//		printf("blikled: %d \n", uxTaskGetStackHighWaterMark(task3hadle));
+//		vTaskDelay(1000 / portTICK_PERIOD_MS);
+//	}
+//}
 void app_main()
 {
-	xTaskCreate(vText1, "text1", 512, NULL, 1, &task1hadle);
-	xTaskCreate(vText2, "text2", 512, NULL, 1, &task2hadle);
+	xTaskCreate(vText1, "text1", 2048, NULL, 1, &Print1Handle);
+//	xTaskCreate(vText2, "text2", 512, NULL, 1, &Print2Handle);
 	xTaskCreate(vPrintFreeMemory, "printfreememory", 4096, NULL, 2, NULL);
-	xTaskCreate(vBlink_Led2, "blik led2", 1500, NULL, 1, task3hadle);
+	xTaskCreate(vBlink_Led2, "blik led2", 1500, NULL, 1, &BlikLedHandle);
 
 
 }
