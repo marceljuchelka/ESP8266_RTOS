7a0d036e26b3441013eaecda592083a99d3b815b
marcel <marcel@strihovy>
2022-05-29 21:07:39 +0200

    test tasku

--- rtos_pokus/main/rtos_pokus.c
+++ rtos_pokus/main/rtos_pokus.c
@@ -12,12 +12,14 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
-#include "esp_spi_flash.h"
-#include "esp_flash_partitions.h"
+//#include "esp_spi_flash.h"
+//#include "esp_flash_partitions.h"
+#include "esp_task_wdt.h"
 #include "esp_log.h"
 #include "driver/gpio.h"
 #include "portmacro.h"
 #include "sdkconfig.h"
+
 
 TaskHandle_t BlikLedHandle;
 TaskHandle_t Print1Handle;
@@ -41,7 +43,7 @@ void vBlink_Led2(void *arg){
 
 	while(1){
 		gpio_set_level(GPIO_NUM_2, uroven=uroven^1);
-		vTaskDelay(1);
+		vTaskDelay(100);
 
 	}
 }
@@ -49,17 +51,19 @@ void vBlink_Led2(void *arg){
 void vPrintFreeMemory(void *arg) {
 	while (1) {
 		ESP_LOGI("Free Mem:","%d\n", esp_get_free_heap_size());
-		vTaskDelay(100 / portTICK_PERIOD_MS);
+		vTaskDelay(50 / portTICK_PERIOD_MS);
 	}
 }
 
 void vText2(void *arg) {
 	ESP_LOGI("print2","created");
 	while (1) {
-		vTaskDelay(300 / portTICK_PERIOD_MS);
 		printf("Text2 \n");
-		ESP_LOGI("print2","kill");
-		vTaskDelete(Print2Handle);
+//		uint32_t i=0;
+//		while(i--<50000);
+//		vTaskDelay(0 / portTICK_PERIOD_MS);
+//		ESP_LOGI("print2","kill");
+//		vTaskDelete(Print2Handle);
 	}
 }
 
@@ -67,61 +71,70 @@ void vText2(void *arg) {
 void vText1(void *arg) {
 	while (1) {
 		printf("Text1 \n");
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-		xTaskCreate(vText2, "text2", 2048, NULL, 1, &Print2Handle);
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
+//		uint32_t i=0;
+//		while(i--<50000);
+//		vTaskDelay(0);
+//		xTaskCreate(vText2, "text2", 2048, NULL, 1, &Print2Handle);
+//		vTaskDelay(1000 / portTICK_PERIOD_MS);
 //		ESP_LOGI("print2","kill");
 //		vTaskDelete(Print2Handle);
 	}
 }
 
 
-typedef struct {
-	uint8_t pocitadlo;
-	char	text[128];
-} T_FLASH_DATA;
-#define flash_data_base		0x1FC*0x1000		//adresa ulozeni dat
-#define flash_sector_size	0x1000				//velikost sektoru- 4096 Byte
-#define flash_addres 		flash_data_base		//
+//typedef struct {
+//	uint8_t pocitadlo;
+//	char	text[128];
+//} T_FLASH_DATA;
+//#define flash_data_base		0x1FC*0x1000		//adresa ulozeni dat
+//#define flash_sector_size	0x1000				//velikost sektoru- 4096 Byte
+//#define flash_addres 		flash_data_base		//
 
-void save_data_flash(void *arg){
-	esp_err_t esp_error;
-	T_FLASH_DATA flashdata = {.pocitadlo = 23,.text = "Toto je test flash"};
-	printf("save pocitadlo %d text %s\n ",flashdata.pocitadlo, flashdata.text);
-	esp_error = spi_flash_write(flash_addres, (uint32_t*)&flashdata, sizeof(flashdata));
-//	spi_flash_erase_sector(0)
-	if(esp_error == ESP_OK) printf("save OK\n");
-	else printf ("save error %s\n", esp_err_to_name(esp_error));
-}
+//void save_data_flash(void *arg){
+//	esp_err_t esp_error;
+//	T_FLASH_DATA flashdata = {.pocitadlo = 23,.text = "Toto je test flash"};
+//	printf("save pocitadlo %d text %s\n ",flashdata.pocitadlo, flashdata.text);
+//	esp_error = spi_flash_write(flash_addres, (uint32_t*)&flashdata, sizeof(flashdata));
+////	spi_flash_erase_sector(0)
+//	if(esp_error == ESP_OK) printf("save OK\n");
+//	else printf ("save error %s\n", esp_err_to_name(esp_error));
+//}
 
-void read_data_flash(void *arg){
-	esp_err_t esp_error;
-	T_FLASH_DATA flashdata = {.pocitadlo = 0,.text = "neni text"};
-	esp_error = spi_flash_read(flash_addres, (uint32_t*)&flashdata, sizeof(flashdata));
-	if(esp_error == ESP_OK)
-		printf("read pocitadlo %d text %s\n ",flashdata.pocitadlo, flashdata.text);
-	else printf("read error %s\n", esp_err_to_name(esp_error));
-}
+//void read_data_flash(void *arg){
+//	esp_err_t esp_error;
+//	T_FLASH_DATA flashdata = {.pocitadlo = 0,.text = "neni text"};
+//	esp_error = spi_flash_read(flash_addres, (uint32_t*)&flashdata, sizeof(flashdata));
+//	if(esp_error == ESP_OK)
+//		printf("read pocitadlo %d text %s\n ",flashdata.pocitadlo, flashdata.text);
+//	else printf("read error %s\n", esp_err_to_name(esp_error));
+//}
 
 void app_main()
 {
-	printf("velikost flash %d\n", spi_flash_get_chip_size());
+//	printf("velikost flash %d\n", spi_flash_get_chip_size());
 
-	read_data_flash(0);
-	save_data_flash(0);
-	read_data_flash(0);
-	printf("mazani sector 0 %s\n", esp_err_to_name(spi_flash_erase_sector(0x1FC)));
-	read_data_flash(0);
-	save_data_flash(0);
-	read_data_flash(0);
-	vTaskDelay(200);
-	printf("mazani sector 0 %s\n", esp_err_to_name(spi_flash_erase_sector(0x1FC)));
+//	read_data_flash(0);
+//	save_data_flash(0);
+//	read_data_flash(0);
+//	printf("mazani sector 0 %s\n", esp_err_to_name(spi_flash_erase_sector(0x1FC)));
+//	read_data_flash(0);
+//	save_data_flash(0);
+//	read_data_flash(0);
+//	vTaskDelay(200);
+//	printf("mazani sector 0 %s\n", esp_err_to_name(spi_flash_erase_sector(0x1FC)));
+
 
 
 	xTaskCreate(vText1, "text1", 2048, NULL, 1, &Print1Handle);
-//	xTaskCreate(vText2, "text2", 512, NULL, 1, &Print2Handle);
-	xTaskCreate(vPrintFreeMemory, "printfreememory", 4096, NULL, 2, NULL);
-	xTaskCreate(vBlink_Led2, "blik led2", 1500, NULL, 1, &BlikLedHandle);
+	xTaskCreate(vText2, "text2", 2048, NULL, 1, &Print2Handle);
+	xTaskCreate(vPrintFreeMemory, "printfreememory", 4096, NULL, 1, NULL);
+	xTaskCreate(vBlink_Led2, "blik led2", 2048, NULL, 1, &BlikLedHandle);
+
+	while(1){
+
+		esp_task_wdt_reset();
+
+	}
 
 
 }
