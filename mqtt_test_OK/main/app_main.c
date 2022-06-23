19f7405c3e3331763f594fd20f91e74fc89d9f40
marcel <marcel@PC-Marcel>
2022-06-23 14:05:50 +0200

    oprava

--- mqtt_test_OK/main/app_main.c
+++ mqtt_test_OK/main/app_main.c
@@ -9,19 +9,21 @@
 
 #include <stdio.h>
 #include <stdint.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "freertos/queue.h"
 #include <stddef.h>
 #include <string.h>
 #include "esp_wifi.h"
 #include "esp_system.h"
+
 #include "nvs_flash.h"
 #include "esp_event.h"
 #include "esp_netif.h"
 #include "protocol_examples_common.h"
 
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/semphr.h"
-#include "freertos/queue.h"
+
 
 #include "lwip/sockets.h"
 #include "lwip/dns.h"
@@ -29,8 +31,9 @@
 
 #include "esp_log.h"
 #include "mqtt_client.h"
-
+#include "sdkconfig.h"
 static const char *TAG = "MQTT_EXAMPLE";
+
 
 
 static esp_err_t mqtt_event_handler_cb(esp_mqtt_event_handle_t event)
